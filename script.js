4d851f9dd061e97d90448da0bb76c4fe51690a84
tokurvadim <122287889+tokurvadim@users.noreply.github.com>
2023-06-04 19:54:55 +0300

    Update script.js
    delete useful code

--- script.js
+++ script.js
@@ -13,26 +13,3 @@ function previousImage(){
     if (counter < 0) counter = imageArray.length - 1;
     image.src = `images/${imageArray[counter]}`;
 }
-
-
-
-
-function next() {
-    const image = document.getElementById("image");
-    const imageAfter = document.getElementById("image__after")
-    imageAfter.display = "block";
-    imageAfter.src = `images/${imageArray[counter]}`;
-    counter++;
-    if (counter > imageArray.length - 1) counter = 0;
-    image.transform = "translateX(-1000px)";
-    image.src = `images/${imageArray[counter]}`;
-    imageAfter.right = "0";
-    image.animationName = "next";
-    imageAfter.animationName = "next";
-    setTimeout(() => {
-        image.style.animationName = "none";
-        imageAfter.animationName = "none";
-    }, 1000);
-    imageAfter.right = "translateX(-1000px)";
-    imageAfter.display = "none";
-}
